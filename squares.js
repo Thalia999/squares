152abc24f62f83a1cf19d0a1c17ee9cdc0c7d6bf
Thalia999 <thaliac000@gmail.com>
2026-04-25 19:50:02 -0400

    functions that add square and change colors

--- squares.js
+++ squares.js
@@ -1,7 +1,7 @@
 let squareArea =document.querySelector("#squarearea");
 //let squareArea =document.getElementById("squarearea");
 let squareCounter = parseInt(Math.random()*21)+30; //30-50
-
+//make some variable for a big z index e.g. 1000
 for (let i=0; i<squareCounter; i++){
     //make square
     let square = document.createElement("div");
@@ -9,6 +9,7 @@ for (let i=0; i<squareCounter; i++){
     square.style.left = parseInt(Math.random()*650)+"px";
     square.style.top = parseInt(Math.random()*250)+"px";
     square.style.backgroundColor = getRandomColor();
+    //put onclick and func(this)
     //put it on screen
     squareArea.append(square);
 }
@@ -22,4 +23,18 @@ function getRandomColor(){
     }
     //return the color string
     return color;
+}
+//make a func to add 1 square
+//make sure that I set as onclick
+
+//make a func that changes color of all squares
+//make sure button calls it
+function changeColors(){
+    //make an array
+    let allSquares = document.querySelectorAll(".square");
+    //for of loop or reg for loop with index
+    //change the color
+    //1-3 lines of code 
+    
+
 }
